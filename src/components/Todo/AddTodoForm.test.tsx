544f772beddb75da0499d468ac55c04e9012f1ec
Edward <edward.nayve@boom.ai>
2025-03-18 23:24:41 +0800

    Add test on todo item

---
+++ src/components/Todo/AddTodoForm.test.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import { render, screen, fireEvent } from "@testing-library/react"
+
+import { TodoProvider } from "../../context/TodoContext";
+
+import AddTodoForm from "./AddTodoForm";
+
+
+describe('AddTodoForm', () => {
+    test('renders input and button', () => {
+        render(
+            <TodoProvider>
+                <AddTodoForm />
+            </TodoProvider>
+        )
+        
+        expect(screen.getByPlaceholderText('Add a new todo')).toBeInTheDocument();
+        expect(screen.getByText('Add')).toBeInTheDocument();
+    })
+
+    test('allow typing in the input field', () => {
+        render(
+            <TodoProvider>
+                <AddTodoForm />
+            </TodoProvider>
+        )
+        
+        const input = screen.getByPlaceholderText('Add a new todo')
+        fireEvent.change(input, { target: { value: 'New test todo'}})
+
+        expect(input).toHaveValue('New test todo')
+    })
+
+    test('clears input after submitting the form', () => {
+        render(
+            <TodoProvider>
+                <AddTodoForm />
+            </TodoProvider>
+        )
+
+        const input = screen.getByPlaceholderText('Add a new todo')
+        fireEvent.change(input, { target: { value: 'New test todo'}})
+        fireEvent.submit(input.closest('form')!)
+
+        expect(input).toHaveValue('')
+    })
+})
---
+++ src/components/Todo/TodoItem.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { render, screen, fireEvent } from "@testing-library/react"
+
+import { TodoProvider } from "../../context/TodoContext";
+
+import TodoItem from "./TodoItem";
+
+
+describe('TodoItem', () => {
+    const mockTodo = {
+        id: 1,
+        text: 'Mock todo',
+        completed: false
+    };
+
+    test('renders todo text', () => {
+        render(
+            <TodoProvider>
+                <TodoItem todo={mockTodo} />
+            </TodoProvider>
+        )
+
+        expect(screen.getByText('Mock todo')).toBeInTheDocument();
+    });
+
+    test('toggle completion when button was clicked', () => {
+        render(
+            <TodoProvider>
+                <TodoItem todo={mockTodo} />
+            </TodoProvider>
+        )
+
+        const checkbox = screen.getByRole('checkbox')
+        expect(checkbox).not.toBeChecked()
+
+        fireEvent.click(checkbox)
+
+    })
+})
